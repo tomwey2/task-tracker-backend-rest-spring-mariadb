a8683ad6f93e874c2bba7bf0057ddbab651139cd
Thomas <tomwey2@gmail.com>
2025-06-16 09:15:57 +0400

    spring initializr

---
+++ src/main/java/de/tomwey2/taskapp_backend/TaskappBackendApplication.java
@@ -0,0 +1,13 @@
+package de.tomwey2.taskapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tomwey2/taskapp_backend/TaskappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tomwey2.taskapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
