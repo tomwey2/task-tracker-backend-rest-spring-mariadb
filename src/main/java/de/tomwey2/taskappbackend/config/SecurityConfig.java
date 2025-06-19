770ea07a86fa2131ac798dcc79b1ca9cc88cb110
Thomas <tomwey2@gmail.com>
2025-06-19 17:12:28 +0400

    added swagger ui

--- src/main/java/de/tomwey2/taskappbackend/config/SecurityConfig.java
+++ src/main/java/de/tomwey2/taskappbackend/config/SecurityConfig.java
@@ -1,17 +1,19 @@
 package de.tomwey2.taskappbackend.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 // Die moderne Art, Spring Security zu konfigurieren (seit Spring Boot 3), ist über eine Konfigurationsklasse,
@@ -58,6 +60,8 @@ public class SecurityConfig {
 
                 // 2. Die Autorisierungsregeln für HTTP-Requests definieren.
                 .authorizeHttpRequests(authz -> authz
+                        // Regeln, um die OpenAPI-Dokumentation öffentlich zu machen
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                         // Jede GET-Anfrage, die auf /api/** passt, ist für jeden erlaubt.
                         .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                         // Jede andere Anfrage an die API erfordert eine Authentifizierung.
@@ -68,5 +72,32 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    /**
+     * Globale Informationen und Security konfigurieren
+     * Globale Informationen, wie den Titel der API, die Version und die Security-Konfigurationen.
+     * Auf der Swagger UI oben rechts befindet sich jetzt ein "Authorize"-Button.
+     * Dort kann man user/password-Credentials eingeben, und Swagger UI wird sie automatisch
+     * für die Test-Anfragen an die geschützten Endpunkte verwenden.
+     *
+     * @return
+     */
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "basicAuth";
+        return new OpenAPI()
+                .info(new Info().title("Task Tracker Backend API")
+                        .version("1.0")
+                        .description("API für das Task Tracker Backend.")
+                        .license(new License().name("Apache 2.0").url("https://github.com/tomwey2/task-tracker-backend-rest-spring-mariadb")))
+                // Füge die Security-Definition für Basic Auth hinzu
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("basic")
+                                .description("HTTP Basic Authentication")));
     }
 }
--- src/main/java/de/tomwey2/taskappbackend/controller/TaskController.java
+++ src/main/java/de/tomwey2/taskappbackend/controller/TaskController.java
@@ -5,6 +5,12 @@ import de.tomwey2.taskappbackend.dto.TaskResponseDto;
 import de.tomwey2.taskappbackend.model.Task;
 import de.tomwey2.taskappbackend.dto.TaskModelAssembler;
 import de.tomwey2.taskappbackend.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
@@ -20,15 +26,25 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 @RestController
 @RequestMapping("/api") // Basis-URL für alle Endpunkte in diesem Controller
 @RequiredArgsConstructor
+@Tag(name = "Tasks", description = "Operations that affect tasks")
 public class TaskController {
 
     private final TaskService taskService;
     private final TaskModelAssembler taskModelAssembler;
 
-    // GET /api/tasks -> Alle Tasks abrufen
+    @Operation(
+            summary = "Search for tasks",
+            description = "Retrieves a list of tasks, optionally filtered by project ID and assigned user." +
+                    "If no parameter is specified, all tasks are returned.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+    })
     @GetMapping("/tasks")
     public CollectionModel<EntityModel<TaskResponseDto>> findTasks(
+            @Parameter(description = "ID of the project to filter by")  // Swagger-UI
             @RequestParam(name = "projectId", required = false) Long projectId,
+            @Parameter(description = "ID of the user to whom the tasks are assigned")
             @RequestParam(name = "assignedToUserId", required = false) Long assignedToUserId) {
 
         // Wenn beide Parameter null sind, funktioniert dies wie eine "findAll"-Abfrage.
@@ -41,9 +57,17 @@ public class TaskController {
                 .add(linkTo(methodOn(TaskController.class).findTasks(projectId, assignedToUserId)).withSelfRel());
     }
 
-    // GET /api/tasks/{id} -> Einen Task anhand seiner ID abrufen
+    @Operation(
+            summary = "Get a task with a given ID",
+            description = "Retrieves a task with a given task ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Task with ID Not Found", content = @Content)
+    })
     @GetMapping("/tasks/{id}")
-    public ResponseEntity<EntityModel<TaskResponseDto>>  getTaskById(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<TaskResponseDto>> getTaskById(
+            @Parameter(description = "ID of the task")  // Swagger-UI
+            @PathVariable Long id) {
         return taskService.getTaskById(id)
                 .map(taskModelAssembler::toModel)
                 .map(ResponseEntity::ok)
@@ -51,8 +75,16 @@ public class TaskController {
     }
 
     // POST /api/users/{userId}/tasks -> Erstellt einen neuen Task. Der User userId ist der Reporter
+    @Operation(
+            summary = "Create a new task",
+            description = "Creates a new task. The user ID is the reporter of the task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "404", description = "User with ID Not Found"),
+    })
     @PostMapping("/users/{userId}/tasks")
     public ResponseEntity<EntityModel<TaskResponseDto>> createTask(
+            @Parameter(description = "ID of the user")  // Swagger-UI
             @PathVariable Long userId,
             @Valid @RequestBody TaskRequestDto taskRequest) {
 
@@ -60,9 +92,18 @@ public class TaskController {
         return new ResponseEntity<>(taskModelAssembler.toModel(task), HttpStatus.CREATED);
     }
 
-    // PUT /api/tasks/{id} -> Einen bestehenden Task aktualisieren
+    @Operation(
+            summary = "Update the task with a given ID",
+            description = "Updates the task with a given ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Task with ID Not Found"),
+    })
     @PutMapping("/tasks/{id}")
-    public ResponseEntity<EntityModel<TaskResponseDto>> updateTask(@PathVariable Long id, @RequestBody TaskRequestDto taskRequest) {
+    public ResponseEntity<EntityModel<TaskResponseDto>> updateTask(
+            @Parameter(description = "ID of the task")  // Swagger-UI
+            @PathVariable Long id,
+            @RequestBody TaskRequestDto taskRequest) {
         return taskService.updateTask(id, taskRequest)
                 .map(taskModelAssembler::toModel)
                 .map(ResponseEntity::ok)
@@ -70,8 +111,17 @@ public class TaskController {
     }
 
     // DELETE /api/tasks/{id} -> Einen Task löschen
+    @Operation(
+            summary = "Delete the task with a given ID",
+            description = "Deletes the task with a given ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No Content"),
+            @ApiResponse(responseCode = "404", description = "Task with ID Not Found"),
+    })
     @DeleteMapping("/tasks/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteTask(
+            @Parameter(description = "ID of the task")  // Swagger-UI
+            @PathVariable Long id) {
         if (taskService.deleteTask(id)) {
             return ResponseEntity.noContent().build(); // Status 204 No Content
         } else {
